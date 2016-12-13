2f2664a0fd59ab77a930dcc443aa2e37eab45113
bencode <bencode@163.com>
2016-12-13 22:16:19 +0800

    add chris

---
+++ static/public/chris/assets/chris_actions.js
@@ -0,0 +1,13 @@
+var flwebgl;
+(function (flwebgl) {
+	(function (actions) {
+		actions.mc_lover247pause45btn_0_0 = function() {
+			/* this.parent.stop();
+			
+			var self = this;
+			this.btn.addEventListener("click", function() {
+			   self.parent.play();
+			});*/
+		}
+	})(flwebgl.actions || (flwebgl.actions = {}));
+})(flwebgl || (flwebgl = {}));
