e534485317535874f2d590c02eb76e1ed6da6cdf
bencode <bencode@163.com>
2016-12-14 12:05:01 +0800

    临时保存

--- static/public/chris/chris.js
+++ static/public/chris/chris.js
@@ -1,32 +1,18 @@
 function setup() {
   var sgf = player.getScenegraphFactory();
   var curtain = sgf.createMovieClipInstance('curtain');
-  var start = sgf.createMovieClipInstance('start');
 
   var stage = player.getStage();
 
-  stage.addChild(start);
   stage.addChild(curtain);
-
   curtain.stop();
-  start.setVisible(false);
-  start.stop();
 
-  const enter = document.querySelector('.enter');
-  enter.addEventListener('click', function() {
-    start.setVisible(true);
-    start.play();
+
+  const btn = document.querySelector('.next');
+  btn.addEventListener('click', function() {
+    next();
   });
 
-
-  var flag = false;
-  start.addEventListener(flwebgl.events.Event.EXIT_FRAME, function() {
-    if (!flag && start.getCurrentFrame() === start.getTotalFrames()) {
-      flag = true;
-      start.stop();
-      curtain.play();
-    }
-  });
 
   curtain.addEventListener(flwebgl.events.Event.ENTER_FRAME, function() {
     if (curtain.getCurrentFrame() > 15) {
@@ -35,3 +21,7 @@ function setup() {
   });
 }
 
+
+function next() {
+  console.log('next');
+}
